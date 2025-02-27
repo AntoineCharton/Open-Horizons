d332da1b942dabfb04617f213d3f8961d8bd80e2
Antoine Charton <antoine.charton@gmail.com>
2025-02-26 23:13:22 -0500

    Add set world position function

--- com.antoine.openhorizons/BigWorld/Runtime/ReferenceTransform.cs
+++ com.antoine.openhorizons/BigWorld/Runtime/ReferenceTransform.cs
@@ -15,6 +15,13 @@ namespace BigWorld
                 referencePosition.Z + localPosition.Z);
         }
 
+        public void SetWorldPosition(DoubleVector3 worldPosition)
+        {
+            referencePosition = worldPosition;
+            GetRootParent(transform).position = Vector3.zero;
+            localPosition = new DoubleVector3(Vector3.zero);
+        }
+
         private void FixedUpdate()
         {
             localPosition = new DoubleVector3(transform.position);
